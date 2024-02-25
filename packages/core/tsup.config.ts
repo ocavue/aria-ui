ca0507e03927fd7f9d0488ea07dc554974e7340a
ocavue <ocavue@gmail.com>
2024-02-25 18:55:31 +0800

    fix build

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  // dts: true,
   format: "esm",
   dts: {
     entry: "src/index.ts",
-    compilerOptions: { composite: false },
+    compilerOptions: {
+      composite: false,
+    },
   },
 })
