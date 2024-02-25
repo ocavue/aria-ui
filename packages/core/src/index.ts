798bb6fdbb2afa1f87c9448423bfbda3791fee45
ocavue <ocavue@gmail.com>
2024-02-25 19:00:37 +0800

    fix export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,26 +1,26 @@
 export { BaseElement } from "./base-element"
+export type { ConnectableElement } from "./connectable-element"
+export { createContext, type Context } from "./context"
 export {
-  type ReadonlySignal,
-  type Signal,
-  type SignalValue,
-  useEffect,
+  useAriaAttribute,
+  useAriaRole,
+  useAttribute,
+  useEventListener,
+  useStyle,
+} from "./dom"
+export { assignProps } from "./props"
+export {
   batch,
   createComputed,
   createSignal,
   untracked,
+  useEffect,
+  type ReadonlySignal,
+  type Signal,
+  type SignalValue,
 } from "./signals"
-export type { ConnectableElement } from "./connectable-element"
 export {
-  type SingalState,
-  mapValues,
   mapSignals,
+  mapValues,
+  type SingalState,
 } from "./singal-state"
-export { assignProps } from "./props"
-export {
-  useEventListener,
-  useStyle,
-  useAttribute,
-  useAriaAttribute,
-  useAriaRole,
-} from "./dom"
-export { createContext, type Context } from "./context"
