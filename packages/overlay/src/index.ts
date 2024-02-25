ae237d1d723b5bcd77ddfb28b537e5518354bb2b
ocavue <ocavue@users.noreply.github.com>
2024-02-26 00:34:26 +0800

    docs: add module description (#10)

--- packages/overlay/src/index.ts
+++ packages/overlay/src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * A collection of low-level utilities for creating custom elements that display
+ * floating content.
+ *
+ * You probably won't need to use this module directly.
+ *
+ * @module
+ */
+
 export { useOverlayAnchor } from "./overlay-anchor-state"
 export {
   defaultOverlayPositionerProps,
