84f5a8f79d737b1a79c22ba2c281034139f20159
ocavue <ocavue@users.noreply.github.com>
2024-02-27 20:30:00 +0800

    docs: setup website (#13)

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,13 @@ import { basic } from "@ocavue/eslint-config"
 
 export default [
   ...basic(),
-  // Remove the ignore once this PR is merged
-  // https://github.com/tgreyuk/typedoc-plugin-markdown/pull/579
-  { ignores: ["packages/typedoc-plugin/"] },
+  {
+    ignores: [
+      "**/.astro",
+
+      // TODO: Remove the ignore once this PR is merged and released
+      // https://github.com/tgreyuk/typedoc-plugin-markdown/pull/579
+      "packages/typedoc-plugin/",
+    ],
+  },
 ]
---
+++ website/src/content/docs/guides/introduction.md
@@ -0,0 +1,5 @@
+---
+title: Introduction
+---
+
+**Aria UI** is an open-source collection of unstyled Web Components and tools that helps you build accessible user interface.
