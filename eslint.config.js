83c268610a356bffdd0f19b5eb4c6677e5759e5e
ocavue <ocavue@users.noreply.github.com>
2024-03-27 14:39:22 +1100

    chore: update generate script for tsconfig.json and typedoc.json (#35)

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,21 @@
-import { basic } from "@ocavue/eslint-config"
+import { basic, tsPlugin } from "@ocavue/eslint-config"
 
 export default [
   ...basic(),
+  {
+    files: ["scripts/**/*.ts"],
+    plugins: {
+      "@typescript-eslint/": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "import/no-extraneous-dependencies": "off",
+    },
+  },
   {
     ignores: [
       "**/.astro",
---
+++ scripts/gen-config.ts
@@ -0,0 +1,52 @@
+import path from "node:path"
+
+import CommentJSON from "comment-json"
+
+import { listGitFiles } from "./list-git-files"
+import { root } from "./root"
+
+async function genRootTypedoc() {
+  const files = await listGitFiles()
+  const rootTypedocPath = path.join(root, "typedoc.json")
+  const rootTypedocConfig = CommentJSON.parse(
+    await Bun.file(rootTypedocPath).text(),
+    // biome-ignore lint/suspicious/noExplicitAny: allow any
+  ) as any
+  rootTypedocConfig.entryPoints = files
+    .map((filePath) => path.relative(root, filePath))
+    .filter((filePath) => filePath.endsWith("typedoc.json"))
+    .filter((filePath) => filePath !== "typedoc.json")
+    .map((filePath) => path.dirname(filePath))
+    .sort()
+  await Bun.write(
+    rootTypedocPath,
+    CommentJSON.stringify(rootTypedocConfig, null, 2),
+  )
+}
+
+async function genRootTsconfig() {
+  const files = await listGitFiles()
+  const rootTsconfigPath = path.join(root, "tsconfig.json")
+  const rootTsconfigConfig = CommentJSON.parse(
+    await Bun.file(rootTsconfigPath).text(),
+    // biome-ignore lint/suspicious/noExplicitAny: allow any
+  ) as any
+  rootTsconfigConfig.references = files
+    .map((filePath) => path.relative(root, filePath))
+    .filter((filePath) => filePath.endsWith("tsconfig.json"))
+    .filter((filePath) => filePath !== "tsconfig.json")
+    .map((filePath) => path.dirname(filePath))
+    .sort()
+    .map((dirPath) => ({ path: dirPath }))
+  await Bun.write(
+    rootTsconfigPath,
+    CommentJSON.stringify(rootTsconfigConfig, null, 2),
+  )
+}
+
+async function main() {
+  await genRootTypedoc()
+  await genRootTsconfig()
+}
+
+void main()
