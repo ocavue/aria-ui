4f3a64a31a45ffeb00b909ffed4e9d6ab1575436
ocavue <ocavue@users.noreply.github.com>
2024-06-16 19:32:23 +1000

    test: setup testing (#102)

---
+++ packages/config/src/vitest-workspace.js
@@ -0,0 +1,16 @@
+import { defineProject } from "vitest/config"
+
+/** @type {import('vitest').UserWorkspaceConfig} */
+const config = defineProject({
+  test: {
+    setupFiles: ["@aria-ui/config/vitest-setup"],
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      name: "chromium",
+      headless: !!process.env.CI,
+    },
+  },
+})
+
+export { config }
