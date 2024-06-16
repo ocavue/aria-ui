7be220955a54373246990d4f278892ea6430b75e
ocavue <ocavue@users.noreply.github.com>
2024-06-16 20:22:08 +1000

    fix(popover): `onOpenChange` should only be triggered with user interactions (#104)

--- packages/popover/src/popover-root.context.ts
+++ packages/popover/src/popover-root.context.ts
@@ -1,6 +1,11 @@
 import { createContext } from "@aria-ui/core"
 
 export const openContext = createContext<boolean>("popover/open", false)
+
+export const onOpenChangeContext = createContext<
+  ((open: boolean) => void) | null
+>("popover/onOpenChange", null)
+
 export const triggerElementContext = createContext<HTMLElement | null>(
   "popover/triggerElement",
   null,
