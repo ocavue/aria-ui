d47b52d47d97ad630ca3c5c4150717ff7a986328
ocavue <ocavue@users.noreply.github.com>
2024-08-22 17:35:37 +1000

    chore: use astrobook (#140)

--- website/src/env.d.ts
+++ website/src/env.d.ts
@@ -2,3 +2,8 @@
 
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "*.astro" {
+  const value: () => unknown
+  export default value
+}
---
+++ website/src/select.stories.ts
@@ -0,0 +1,7 @@
+import SelectPreview from "./components/select-preview.astro"
+
+export default {
+  component: SelectPreview,
+}
+
+export const Default = {}
