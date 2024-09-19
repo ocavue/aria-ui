881b1f8ec74c06892c360389c18cc4d4c92b6146
ocavue <ocavue@gmail.com>
2024-09-19 14:43:30 +1000

    fix

--- packages/menu/src/index.ts
+++ packages/menu/src/index.ts
@@ -1,10 +1,8 @@
 import { registerCustomElement } from "@aria-ui/core"
-
 import { MenuContentElement } from "./menu-content.element.gen"
 import { MenuItemElement } from "./menu-item.element.gen"
 import { MenuRootElement } from "./menu-root.element.gen"
 import { MenuTriggerElement } from "./menu-trigger.element.gen"
-
 export * from "./menu-content.types"
 export * from "./menu-item.types"
 export * from "./menu-root.types"
--- packages/overlay/src/index.ts
+++ packages/overlay/src/index.ts
@@ -1,9 +1,7 @@
 import { registerCustomElement } from "@aria-ui/core"
-
 import { OverlayAnchorElement } from "./overlay-anchor.element.gen"
 import { OverlayPositionerElement } from "./overlay-positioner.element.gen"
 import { OverlayRootElement } from "./overlay-root.element.gen"
-
 export * from "./overlay-anchor.types"
 export * from "./overlay-positioner.types"
 export * from "./overlay-root.types"
