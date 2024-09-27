42af0c59580498688ac8d6a999510cbe093fadc1
ocavue <ocavue@gmail.com>
2024-09-27 17:23:14 +1000

    fix

--- packages/menu/src/index.ts
+++ packages/menu/src/index.ts
@@ -9,6 +9,7 @@ export * from "./menu-content.types"
 export * from "./menu-item.types"
 export * from "./menu-root.types"
 export * from "./menu-trigger.types"
+
 registerCustomElement("aria-ui-menu-content", MenuContentElement)
 registerCustomElement("aria-ui-menu-item", MenuItemElement)
 registerCustomElement("aria-ui-menu-root", MenuRootElement)
