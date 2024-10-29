7c81d6bad03f2c318bc1bf8aac0559321d78afc5
ocavue <ocavue@gmail.com>
2024-10-29 16:56:19 +1100

    update docs

--- packages/overlay/src/overlay-positioner.types.ts
+++ packages/overlay/src/overlay-positioner.types.ts
@@ -46,10 +46,9 @@ export interface OverlayPositionerProps {
   hoist: boolean
 
   /**
-   * Whether to use CSS transforms (`transform: translate3d()`) to position the
-   * floating element instead of layout (`top` and `left` CSS properties). CSS
-   * transforms are more performant, but can cause conflicts with transform
-   * animations.
+   * Whether to use CSS transforms to position the floating element instead of
+   * layout (`top` and `left` CSS properties). CSS transforms are more
+   * performant, but can cause conflicts with transform animations.
    *
    * @default false
    */
