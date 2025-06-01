6b7b4435ef242704986b2388704701acbc4781a0
ocavue <ocavue@users.noreply.github.com>
2025-06-01 15:10:36 +1000

    chore(deps): update dependencies (#194)

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,12 @@
-import { basic } from "@ocavue/eslint-config"
+import { defineESLintConfig } from "@ocavue/eslint-config"
 
-export default [
-  ...basic(),
+export default defineESLintConfig({}, [
   {
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/unbound-method": "off",
-      "import/no-extraneous-dependencies": "off",
     },
   },
-  {
-    ignores: ["**/.astro"],
-  },
-]
+])
