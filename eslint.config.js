17f8ac3838d18b71e6d7814971ca9946671b0c9b
ocavue <ocavue@gmail.com>
2025-06-01 15:05:23 +1000

    remove log

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ export default defineESLintConfig({}, [
   {
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
