0170737090a73ae674ff1c8df3957d90d11ce138
ocavue <ocavue@users.noreply.github.com>
2026-04-08 02:36:16 +0800

    feat(utils): add `getNearestOverflowAncestor` (#243)

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
+export { getNearestOverflowAncestor } from '@zag-js/dom-query'
 export {
   getAriaHasPopup,
   setAriaHasPopup,
@@ -17,10 +18,10 @@ export { createDelayedToggle, type DelayedToggle } from './delayed-toggle.ts'
 export * as FeatureDetectionInternals from './feature-detection/feature-detection-internals.ts'
 export * as FeatureDetection from './feature-detection/feature-detection.ts'
 export { useAttribute } from './use-attribute.ts'
+export { useDataState } from './use-data-state.ts'
 export { useDisabledMountTransitionStyle } from './use-disabled-mount-transition-style.ts'
 export { useElementId } from './use-element-id.ts'
 export { useHover, type UseHoverOptions } from './use-hover.ts'
 export { usePresence } from './use-presence.ts'
 export { usePress } from './use-press.ts'
 export { useTransitionStatus, type TransitionStatus } from './use-transition-status.ts'
-export { useDataState } from './use-data-state.ts'
