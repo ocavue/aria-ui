cf332d02434e6e9136f6ea37bd6d4d9d3a359b1f
ocavue <ocavue@gmail.com>
2026-04-10 15:03:41 +1000

    fix

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   integrations: [
     astrobook({
       title: 'aria-ui',
+      homeContent: {
+        subtitle: false,
+        title: 'aria-ui',
+        repo: { href: 'https://github.com/ocavue/aria-ui' },
+        version: false,
+      },
       css: ['./src/styles/tailwind.css'],
     }),
   ],
