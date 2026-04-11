64cacacecf65f26674459ff969a3deef06028271
ocavue <ocavue@gmail.com>
2026-04-11 23:47:48 +1000

    w

--- packages/elements/src/tooltip/index.ts
+++ packages/elements/src/tooltip/index.ts
@@ -1,22 +1,17 @@
-
-
-import {
-  OpenChangeEvent,
-  registerTooltipRootElement,
-  setupTooltipRoot,
-  TooltipRootElement,
-  TooltipRootPropsDeclaration,
-  type TooltipRootEvents,
-  type TooltipRootProps,
-} from './tooltip-root.ts'
-import {
-  registerTooltipTriggerElement,
-  setupTooltipTrigger,
-  TooltipTriggerElement,
-  TooltipTriggerPropsDeclaration,
-  type TooltipTriggerProps,
-} from './tooltip-trigger.ts'
-
+export {
+  registerTooltipPopupElement,
+  setupTooltipPopup,
+  TooltipPopupElement,
+  TooltipPopupPropsDeclaration,
+  type TooltipPopupProps,
+} from './tooltip-popup.ts'
+export {
+  registerTooltipPositionerElement,
+  setupTooltipPositioner,
+  TooltipPositionerElement,
+  TooltipPositionerPropsDeclaration,
+  type TooltipPositionerProps,
+} from './tooltip-positioner.ts'
 export {
   OpenChangeEvent,
   registerTooltipRootElement,
@@ -25,22 +20,11 @@ export {
   TooltipRootPropsDeclaration,
   type TooltipRootEvents,
   type TooltipRootProps,
+} from './tooltip-root.ts'
+export {
   registerTooltipTriggerElement,
   setupTooltipTrigger,
   TooltipTriggerElement,
   TooltipTriggerPropsDeclaration,
   type TooltipTriggerProps,
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-}
-
-export {registerTooltipPopupElement, setupTooltipPopup, TooltipPopupElement, TooltipPopupPropsDeclaration, type TooltipPopupProps} from './tooltip-popup.ts'
-export {registerTooltipPositionerElement, setupTooltipPositioner, TooltipPositionerElement, TooltipPositionerPropsDeclaration, type TooltipPositionerProps} from './tooltip-positioner.ts'
+} from './tooltip-trigger.ts'
