ca04eb46506f2b27cd709038eae522e95bdeb834
ocavue <ocavue@gmail.com>
2026-05-15 12:59:55 +1000

    refactor: update alien-signals

--- packages/core/src/use-effect.ts
+++ packages/core/src/use-effect.ts
@@ -5,18 +5,6 @@ import { onMount } from './on-mount.ts'
 
 export function useEffect(host: HostElement, callback: () => VoidFunction | void): VoidFunction {
   return onMount(host, () => {
-    let callbackCleanup: VoidFunction | void
-
-    const effectCleanup = effect(() => {
-      callbackCleanup?.()
-      callbackCleanup = undefined
-      callbackCleanup = callback()
-    })
-
-    return () => {
-      callbackCleanup?.()
-      callbackCleanup = undefined
-      effectCleanup()
-    }
+    return effect(callback)
   })
 }
