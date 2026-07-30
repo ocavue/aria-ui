93292b01e0f5c548086175d903177196592b9aff
ocavue <ocavue@gmail.com>
2026-07-30 17:47:13 +1000

    format

--- packages/utils/src/use-press.ts
+++ packages/utils/src/use-press.ts
@@ -24,12 +24,17 @@ export function usePress(host: HostElement, handler: () => void): VoidFunction {
   // https://bugs.webkit.org/show_bug.cgi?id=165004
   // https://bugs.webkit.org/show_bug.cgi?id=311717
   let compositionEndedAt = -1
-  const disposeCompositionEnd = useEventListener(host, 'compositionend', (event) => {
-    compositionEndedAt = event.timeStamp
-  }, {capture:true, passive:true})
+  const disposeCompositionEnd = useEventListener(
+    host,
+    'compositionend',
+    (event) => {
+      compositionEndedAt = event.timeStamp
+    },
+    { capture: true, passive: true },
+  )
 
   const disposeKeyDown = useEventListener(host, 'keydown', (event) => {
-    if (event.isComposing || (event.timeStamp < compositionEndedAt + COMPOSITION_TAIL_MS)) return
+    if (event.isComposing || event.timeStamp < compositionEndedAt + COMPOSITION_TAIL_MS) return
 
     if (event.key === 'Enter' || event.key === ' ') {
       event.preventDefault()
